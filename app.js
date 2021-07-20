57c57e5aaf753139bc802a9b59264137358421f8
marshallwatrous <marshallwatrous95@gmail.com>
2021-07-20 14:01:13 -0700

    Commit 3 - Finishes 3 functions mentioned before and adds 3 variables

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ foodEl.addEventListener("click", feedPet);
 playEl.addEventListener("click", playWithPet);
 sleepEl.addEventListener("click", putPetToSleep);
 
+let hungerLevel = 0;
+let boredomLevel = 0;
+let tirednessLevel = 0;
+
+
 function feedPet() {
-    
+    hungerLevel = 0;
 }
 
 function playWithPet() {
-    
+    boredomLevel = 0;
 }
 
 function putPetToSleep() {
-    
+    tirednessLevel = 0;
 }
+
