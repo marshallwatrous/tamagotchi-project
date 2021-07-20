e9983eae007998a83b1fc9e43927f3d18de99f59
marshallwatrous <marshallwatrous95@gmail.com>
2021-07-20 13:56:23 -0700

    Commit 2 - Adds outlines of feedPet, playWithPet, and putPetToSleep functions

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const foodEl = document.getElementById("food");
 const playEl = document.getElementById("play");
 const sleepEl = document.getElementById("sleep");
+
+foodEl.addEventListener("click", feedPet);
+playEl.addEventListener("click", playWithPet);
+sleepEl.addEventListener("click", putPetToSleep);
+
+function feedPet() {
+    
+}
+
+function playWithPet() {
+    
+}
+
+function putPetToSleep() {
+    
+}
